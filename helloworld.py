df488703b8dc9a081d2c787cc6780817ec1f2c81
Ishanee-Adom <ishaneeadom@gmail.com>
2024-03-23 09:56:57 +0000

    Personal info

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+name=(input("Enter your full name"))
+age=(input("Enter your age"))
+number=(input("Enter your phone number"))
+print("name")
+print("age")
+print("number")
+details={{"name","age","number"}}
+
